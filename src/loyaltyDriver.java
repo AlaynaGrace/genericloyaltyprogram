a06e11db5a74aa85b281243b3a2eff17f2466d8f
DrewMetroState <65046512+DrewMetroState@users.noreply.github.com>
2021-01-31 20:32:37 -0600

    Minor changes to input collection

--- src/loyaltyDriver.java
+++ src/loyaltyDriver.java
@@ -1,42 +1,56 @@
 import java.util.Scanner;
 
 public class loyaltyDriver {
+	
+	public static void main(String[] args) {
 
-	int choice;
-	
-	Customer cus1 = new Customer("Andria", 13549, 0);
-	Customer cus2 = new Customer("Bandalar", 54684, 235);
-	Customer cus3 = new Customer("Casper", 97618, 17);
-	Customer cus4 = new Customer("Durand", 23461, 900);
+		int choice;
+		int custNum;
+		Transaction tC = new Transaction();
 		
-	do {
+		Customer cus1 = new Customer("Andria", 13549, 0);
+		Customer cus2 = new Customer("Bandalar", 54684, 235);
+		Customer cus3 = new Customer("Casper", 97618, 17);
+		Customer cus4 = new Customer("Durand", 23461, 900);
+		
 		System.out.println("Welcome to the reward system menu.");
-		System.out.println("Please select from the following options.");
-		System.out.println("1: Show store");
-		System.out.println("2: Check balance");
-		System.out.println("3: Use points");
-		System.out.println("0: Exit");
+		System.out.println("Please enter your ID number.");
 		
-		try (Scanner in = new Scanner(System.in)) {
-			choice = in.nextInt();
+		Scanner in = new Scanner(System.in);
+		custNum = in.nextInt();
+		
+		//VALID ENTERY CHECK
+			
+		do {				
+			System.out.println("Please select from the following options.");
+			System.out.println("1: Show store");
+			System.out.println("2: Check balance");
+			System.out.println("3: Use points");
+			System.out.println("0: Exit");
+			
+		
+				choice = in.nextInt();
+			
+			
+			//VALID ENTERY CHECK
+			
+			switch(choice) {
+				case 1:
+					tC.showPointShop();
+					break;
+				case 2:
+					
+					break;
+				case 3:
+		
+					break;
+				case 0:
+					break;		
+			}
+			
 		}
+		while(choice != 0);
 		
-		switch(choice) {
-			case 1:
-				
-				break;
-			case 2:
-				
-				break;
-			case 3:
-	
-				break;
-			case 0:
-				break;		
+		System.out.println("Thank you for using the reward system.");
 		}
-		
-	}
-	while(choice != 0);
-	
-	System.out.println("Thank you for using the reward system.");
 }
